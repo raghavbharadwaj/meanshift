c777855d9cfd4aa0182182920afbf7929c33d68f
Raghav <raghavbharadwaj@ensue.us>
2015-09-30 22:57:16 -0700

    Changed Color space for filtering

--- meanshift.py
+++ meanshift.py
@@ -9,7 +9,9 @@ def choose(x):
 	'r':2,
 	'b':3,
 	}[x]
-
+def wait():
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 def create_new(img):
 	height, width, channels = img.shape
 	return np.zeros((height,width,3),np.uint8)
@@ -19,25 +21,29 @@ def meanshift_segment(img,sp,sr,option):
 	i=0
 	if(option == 's'):	
 		while(i<=sp):
+			src = cv2.cvtColor(src,cv2.cv.CV_BGR2Lab,src)
 			dest = create_new(src)
 			cv2.pyrMeanShiftFiltering(src,i,sr,dest)
+			dest=cv2.cvtColor(dest,cv2.cv.CV_Lab2BGR,dest)
 			cv2.imwrite('filtered'+str(i)+'.jpeg',dest)
 			src = dest
 			i=i+10
 	elif(option == 'r'):
 		while(i<=sr):
+			src = cv2.cvtColor(src,cv2.cv.CV_BGR2Lab,src)
 			dest = create_new(src)
 			cv2.pyrMeanShiftFiltering(src,sp,i,dest)
+			dest=cv2.cvtColor(dest,cv2.cv.CV_Lab2BGR,dest)
 			cv2.imwrite('filtered'+str(i)+'.jpeg',dest)
 			src = dest
 			i=i+10
 	elif(option == 'b'):
-		i=0
-		j=0
-		k=0
+		i=0;j=0;k=0
 		while(i<=sp and j<=sr):
+			src = cv2.cvtColor(src,cv2.cv.CV_BGR2Lab,src)
 			dest=create_new(src)
 			cv2.pyrMeanShiftFiltering(src,i,j,dest)
+			dest=cv2.cvtColor(dest,cv2.cv.CV_Lab2BGR,dest)
 			cv2.imwrite('filtered'+str(k)+'.jpeg',dest)
 			src=dest
 			i=i+10
